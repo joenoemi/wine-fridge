9c982ab296a4ae2bc55ef502d91481282a4211ac
joenoemi <joenoemi@hotmail.com>
2026-08-21 19:05:50 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Use only the browser-safe Supabase publishable key or legacy anon key.
 window.CELLAR_CONFIG = {
-  SUPABASE_URL: "https://avvypwbowkslcrhdjcge.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_KMnhbqqJ1EHl5TFsbvUM2A_s0TiZVkd"
+  SUPABASE_URL: "https://qymqdriybsmvtpzjzncd.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_mADb3QNxzSd2NHxrrahMAA_a1dlRBZI"
 };
